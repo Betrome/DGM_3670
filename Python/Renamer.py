a1ad6e4f7b8dd6106129f703c98baa9ce9705a37
Ian Lucas <spiratsu101@gmail.com>
2018-11-07 21:42:00 -0700

    Renamer Complete!

---
+++ Python/Renamer.py
@@ -0,0 +1,24 @@
+import maya.cmds as cmds
+# import maya.mel as mel
+
+def Rename_Main(*args):
+    
+    selected_objects = cmds.ls(selection=True)
+    
+    # specify new name here
+    newname = cmds.textFieldGrp(name, q=1, text=1)
+    
+    for number, object in enumerate(selected_objects):
+        cmds.rename(object, ('%s%02d' % (newname, number)))
+    
+    
+    
+if cmds.window ('Rename_toolWindow', ex=True): 
+     cmds.deleteUI ('Rename_toolWindow')
+
+window = cmds.window( title="Rename_toolWindow", iconName='Renamer', widthHeight=(400, 55) )
+cmds.columnLayout( adjustableColumn=True )
+name = cmds.textFieldGrp( label='Rename:', text='Name')
+cmds.button( label='Execute', command=Rename_Main )
+cmds.setParent( '..' )
+cmds.showWindow( window )
--- Python/randCity.py
+++ Python/randCity.py
@@ -1,13 +1,17 @@
 import maya.cmds as cmds
 from random import uniform as rand
 
+def building (min, max, sXmin, sXmax, sYmin,sYmax, sZmin, sZmax, mXmin, mXmax, mYmin, mYmax, mZmin, mZmax, rXmin, rXmax, rYmin, rYmax, rZmin, rZmax):
 
-result = cmds.ls (sl=True)
-
-num = 20
-
-for i in range (num):
-    cmds.instance(result)
-    cmds.scale(rand(5,-5), rand(5,-5), rand(5,-5), result)
-    cmds.move(rand(10,-10), rand(10,-10), rand(10,-10), result)
-    cmds.rotate(rand(0,360), rand(0,360), rand(0,360), result)    
+    result = cmds.ls (sl=True)
+    
+    num = int(rand(min, max))
+    
+    for i in range (num):
+        cmds.instance(result)
+        cmds.scale(rand(sXmin, sXmax), rand(sYmin, sYmax), rand(sZmin, sZmax), result)
+        cmds.move(rand(mXmin, mXmax), rand(mYmin, mYmax), rand(mZmin, mZmax), result)
+        cmds.rotate(rand(rXmin, rXmax), rand(rYmin, rYmax), rand(rZmin, rZmax), result)    
+        
+        
+building (2, 20, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -5.0, 5.0, -5.0, 5.0, -5.0, 5.0, 0, 180, 0, 180, 0, 180)
