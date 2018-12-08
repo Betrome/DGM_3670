23faef2fd1e2dec09f258a7517bdfa4604e9802b
Ian Lucas <spiratsu101@gmail.com>
2018-12-08 15:00:48 -0700

    CTRL Finished!

--- Python/makeCTRL.py
+++ Python/makeCTRL.py
@@ -1,38 +1,34 @@
 import pymel.core as cmds
 
-def makeCTRL():
+def makeCTRL(*args):
     
     selection = cmds.ls (selection = True)
     
-    size = 0
-    for i in selection:
-        size = i
+    size = len(selection)
     
     if (size == 0):
         cmds.error ("Please select one or more objects.")
         
-    controlSize = cmds.floatSliderGrp (q = 1, v = controlSize)
+    ctrlSize = cmds.floatSliderGrp (controlSize, q = 1, value = 1.0)
     
-    clr = cmds.colorIndexSliderGrp (q = 1, v = controlColor)
+    clr = cmds.colorIndexSliderGrp (controlColor, q = 1, value = 1)
     clr -= 1
 	
-    controlSuff = cmds.textFieldGrp (q = 1, v = controlName)
+    controlSuff = cmds.textFieldGrp (controlName, q = 1, text = 1)
     
-    ctrlShape = cmds.intSliderGrp (q = 1, v = ctrlShape)
-    
-    for i in size:
-        if (cmds.objExists (selection[0] + controlSuff) == 1):
-            cmds.error ("Please enter a different suffix.")
+    shape = cmds.intSliderGrp (ctrlShape, q = 1, value = 1)
 
-    if (controlSuff == ""):
+
+    if controlSuff == "":
         cmds.error ("Please enter a suffix.")
 	    
-    for i in size:
+    i = 0
+    while i < size:
         object1 = []
         objectShape1 = []	  
 
 		#creates Circle controls
-        if (ctrlShape == 0):
+        if shape == 0:
 		    object1 = cmds.circle (nr = (1,0,0), ch = False)
 		    objectShape1 = cmds.pickWalk (direction = 'down')
 		    
@@ -41,7 +37,7 @@ def makeCTRL():
 		    cmds.setAttr (objectShape1[0] + ".overrideColor", clr)		    
 		
 		#Creates Square controls  
-        elif (ctrlShape == 1):
+        elif shape == 1:
             object1 = cmds.nurbsSquare (c = (0, 0, 0), nr = (0, 1, 0), sl1 = 2, sl2 = 2, sps = 1, d = 3, ch = True)
             objectShape1 = cmds.pickWalk (d = 'down')
             objectShape2 = cmds.pickWalk (d = 'right')
@@ -59,7 +55,7 @@ def makeCTRL():
 		       		    		 
 		
 		#creates Sphere controls
-        elif (ctrlShape == 2):
+        elif shape == 2:
 		    
             object1 = cmds.circle (nr = (1, 0, 0), ch = 0)
             objectShape1 = cmds.pickWalk (d = 'down')
@@ -78,12 +74,11 @@ def makeCTRL():
             
             cmds.parent (objectShape3, objectShape2, object1[0], shape = True, r = True)
             cmds.delete (object2[0])
-            cmds.delete (object3[0]) 		    
-
-
-        cmds.rename (object1[0], (selection[i]+ controlSuff))
+            cmds.delete (object3[0])    
+        
+        cmds.rename (object1[0], (selection[i] + controlSuff))
         cmds.select (selection[i] + controlSuff)
-        cmds.scale ((controlSize *.25), (controlSize *.25), (controlSize *.25), r = True)
+        cmds.scale ((ctrlSize *.25), (ctrlSize *.25), (ctrlSize *.25), r = True)
         cmds.makeIdentity (apply = True, t = True, r = True, s = True, n = 0)
         
         #creates an offset node
@@ -96,7 +91,7 @@ def makeCTRL():
         cmds.parent ((selection[i] + controlSuff), (selection[i] + "_offset"))
         
         #parents all controls to the appropriate parent control
-        if (i >= 1):
+        if i >= 1:
             cmds.parent ((selection[i]+"_offset"),  (selection[i-1] + controlSuff))
             
         #sets their attributes to zero
@@ -110,86 +105,42 @@ def makeCTRL():
         
         #connects FK controls directly to joint rotations
         cmds.parentConstraint ((selection[i]+ controlSuff), selection[i], mo = True)
-        
-        
-        
-global def createFKControls():
-    #Start Here
-    if (cmds.window (Title = "makeFKWindow", exists = True))
-	cmds.deleteUI makeFKWindow;
+        i += 1
 
-	string $text;
 
-	$sel = `ls -sl`;	
-    	if (`size ($sel)` >= 1)
-    	
-    	$text = $sel[0];
-    
-    	window -title "Create FK Controllers" makeFKWindow;
-    	columnLayout -adj true makeFKColumn ;
-    
-    
-    
-    	floatSliderGrp 
-    	-label "Control Size" 
-    	-field true
-    	-cw 1  80
-    	-cw 2  40
-    	-cw 3 150
-       -minValue .01 
-    	-maxValue 50.0
-       -fieldMinValue .1 
-    	-fieldMaxValue 50
-       -value 2
-    	controlSize;
-    	
-    	colorIndexSliderGrp 
-    	-cw 1  80
-    	-cw 2  40
-    	-label "Control Color" 
-    	-min 0 
-    	-max 31 
-    	-value 5
-    	controlColor;
-    	
-    	intSliderGrp
-    	-label "Control Shape" 
-    	-field true    	
-    	-cw 1 80
-    	-cw 2 40
-    	-cw 3 80
-    	-minValue 0
-    	-maxValue 2
-       -fieldMinValue 0 
-    	-fieldMaxValue 2
-       -value 0
-       ctrlShape;   	
-    
-        textFieldGrp
-    	-cw 1  80
-    	-cw 2 200
-       -label "Control Suffix"
-       -text  ("_CTRL")
-    	controlName;
 
-    	button
-    	-label "EXECUTE"
-    	-command "makeCTRL"
-    	createFKButton;
-    	
-    	$originalSelection = `ls -sl`;
-    
-        
-        select -r $originalSelection;
-    
-        
-    	showWindow makeFKWindow;
-    	window -edit -wh 310 110 makeFKWindow;
-    	
-    	if (`size($sel)` >= 1)
-        textFieldGrp -e -text "_CTRL" -editable true controlName;
-	
-}
+if cmds.window ('Create_FK_Controllers', ex = True):
+    cmds.deleteUI ('Create_FK_Controllers')
 
-createFKControls
+text = ""
+sel = cmds.ls (sl = True)
+
+size = 0
+for x in sel:
+    size = x
+    
+if size >= 1:
+    text = sel[0]
+    
+window = cmds.window (title = "Create_FK_Controllers", iconName = "Ctrl maker", wh = (310, 110))
+
+cmds.columnLayout (adj = True)
+
+controlSize = cmds.floatSliderGrp (label = "Control Size", field = True, cw3 = (80, 40, 150), minValue = 0.01, maxValue = 50.0, fieldMinValue = 0.1, fieldMaxValue = 50, value = 2)
+
+controlColor = cmds.colorIndexSliderGrp (label = "Control Color", cw3 = (80, 40, 150), min = 0, max = 31, value = 5)
+
+ctrlShape = cmds.intSliderGrp (label = "Control Shape", field = True, cw3 = (80, 40, 80), minValue = 0, maxValue = 2, fieldMinValue = 0, fieldMaxValue = 2, value = 0)
+
+controlName = cmds.textFieldGrp (label = "Control Suffix", text = "_CTRL", cw2 = (80, 200))
+
+createFKButton = cmds.button (label = "EXECUTE", command = makeCTRL)
+
+originalSelection = cmds.ls (sl = True)
+
+cmds.select (originalSelection, r = True)
+
+cmds.showWindow (window)
+
+controlName = cmds.textFieldGrp (text = "_CTRL", editable = True)
 
